2ea82e01ad6f72d03f5921a9fcd5e372258a2ac6
GriMax <ovimaxg@gmail.com>
2025-04-05 14:58:12 +0300

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const WebsiteName: string = "Craft the Fund"
+export const WebsiteBaseUrl: string = ""
+export const WebsiteDescription: string = "It's not much but it's honest work"
+export const CreateProfileStep: boolean = true
